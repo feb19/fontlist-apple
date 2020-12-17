37a248639e82a817dab0ce57604d6f3785772ad5
feb19 <nt@feb19.jp>
2020-12-18 04:32:15 +0900

    Initial Commit

---
+++ Shared/FontListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontListApp.swift
+//  Shared
+//
+//  Created by Nobuhiro Takahashi on 2020/12/18.
+//
+
+import SwiftUI
+
+@main
+struct FontListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
